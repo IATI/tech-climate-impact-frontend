a61ae3286812a86eb6644fccdd439db2e81262dd
nosvalds <niko@devinit.org>
2022-03-03 11:40:50 +0000

    table view setup to pull real data, cypress to intercept

---
+++ cypress/integration/basic.spec.js
@@ -0,0 +1,10 @@
+describe("The TCI Dashboard Homepage", () => {
+  it("Shows the homepage header", () => {
+    cy.visit("/");
+    cy.contains("IATI Technical Climate Impact Dashboard");
+  });
+  it("Gets table data from API and displays", () => {
+    cy.intercept("/api/metrics", { fixture: "metrics" }).as("matchedUrl");
+    cy.visit("/table");
+  });
+});
