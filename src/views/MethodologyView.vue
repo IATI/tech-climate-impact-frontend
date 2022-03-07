e682e6d2da8913013d411a7ba1b35c4688c6c2a6
nosvalds <niko@devinit.org>
2022-03-07 16:58:23 +0000

    add methodology view iframe google doc

--- src/views/MethodologyView.vue
+++ src/views/MethodologyView.vue
@@ -1,5 +1,11 @@
 <script setup></script>
 
 <template>
-  <h2>Methodology Placeholder</h2>
+  <h2 class="text-3xl font-serif font-bold p-4">TCI Methodology</h2>
+  <div class="text-center">
+    <iframe
+      class="w-[800px] h-[900px] block mx-auto my-0"
+      src="https://docs.google.com/document/d/e/2PACX-1vTZki-4WvcGPJGB3L8y7zaGRZOadisVw-f4jJ2JQNVXh1WY27_9XrrkpC99LJEMAx05F9ETD-PW3gPB/pub?embedded=true"
+    ></iframe>
+  </div>
 </template>
