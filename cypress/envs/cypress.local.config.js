e8d2b50f43f209b6308ef7b6f9b75b741d932535
akmiller01 <alex.k.miller@gmail.com>
2023-04-24 08:33:52 -0400

    Remove plugins

--- cypress/envs/cypress.local.config.js
+++ cypress/envs/cypress.local.config.js
@@ -5,11 +5,6 @@ module.exports = defineConfig({
   screenshotsFolder: "cypress/screenshots",
   videosFolder: "cypress/videos",
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require("./cypress/plugins/index.js")(on, config);
-    },
     baseUrl: "http://localhost:3000",
   },
 });
